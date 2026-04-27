0f1980a7003c58b63c26785319d594b1cb581b48
Shobhit Umrao <shobhitumrao5@gmail.com>
2026-04-28 02:13:41 +0530

    show version info for go releases

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,7 +1,34 @@
 package version
 
+import "runtime/debug"
+
 var (
 	Version = "dev"
 	Commit  = "none"
 	Date    = "unknown"
 )
+
+func init() {
+	if Version != "dev" {
+		return
+	}
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return
+	}
+	if info.Main.Version != "" && info.Main.Version != "(devel)" {
+		Version = info.Main.Version
+	}
+	for _, s := range info.Settings {
+		switch s.Key {
+		case "vcs.revision":
+			if len(s.Value) > 12 {
+				Commit = s.Value[:12]
+			} else {
+				Commit = s.Value
+			}
+		case "vcs.time":
+			Date = s.Value
+		}
+	}
+}
